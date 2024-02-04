c026fe67ec050aacfb9fa4978ed49905894cf675
Yang <1228231049@qq.com>
2024-02-04 12:42:03 +0800

    update

---
+++ chapter/desc/main_chapter.go
@@ -0,0 +1,23 @@
+package desc
+
+const (
+	GettingStart     = "1.getting_started"
+	Lighting         = "2.lighting"
+	ModelLoading     = "3.model_loading/1.model_loading"
+	AdvancedOpengl   = "4.advanced_opengl"
+	AdvancedLighting = "5.advanced_lighting"
+	Pbr              = "6.pbr"
+	InPractice       = "7.in_practice"
+	Guest            = "8.guest"
+)
+
+var MainChapterList = []string{
+	GettingStart,
+	Lighting,
+	ModelLoading,
+	AdvancedOpengl,
+	AdvancedLighting,
+	Pbr,
+	InPractice,
+	Guest,
+}
---
+++ chapter/desc/sub_chapter.go
@@ -0,0 +1,124 @@
+package desc
+
+const (
+	ChapterHelloTriangle = "1.hello_triangle"
+	ChapterShader        = "2.shader"
+)
+
+const (
+	ChapterHelloTriangleSub1  = "1.1.hello_window"
+	ChapterHelloTriangleSub2  = "1.2.hello_window_clear"
+	ChapterHelloTriangleSub3  = "2.1.hello_triangle"
+	ChapterHelloTriangleSub4  = "2.2.hello_triangle_indexed"
+	ChapterHelloTriangleSub5  = "2.3.hello_triangle_exercise1"
+	ChapterHelloTriangleSub6  = "2.4.hello_triangle_exercise2"
+	ChapterHelloTriangleSub7  = "2.5.hello_triangle_exercise3"
+	ChapterHelloTriangleSub8  = "3.1.shaders_uniform"
+	ChapterHelloTriangleSub9  = "3.2.shaders_interpolation"
+	ChapterHelloTriangleSub10 = "3.3.shaders_class"
+	ChapterHelloTriangleSub11 = "3.4.shaders_exercise1"
+	ChapterHelloTriangleSub12 = "3.5.shaders_exercise2"
+	ChapterHelloTriangleSub13 = "3.6.shaders_exercise3"
+	ChapterHelloTriangleSub9  = "4.1.textures"
+	ChapterHelloTriangleSub9  = "4.2.textures_combined"
+	ChapterHelloTriangleSub9  = "4.3.textures_exercise1"
+	ChapterHelloTriangleSub9  = "4.4.textures_exercise2"
+	ChapterHelloTriangleSub9  = "4.5.textures_exercise3"
+	ChapterHelloTriangleSub9  = "4.6.textures_exercise4"
+	ChapterHelloTriangleSub9  = "5.1.transformations"
+	ChapterHelloTriangleSub9  = "5.2.transformations_exercise1"
+	ChapterHelloTriangleSub9  = "5.2.transformations_exercise2"
+	ChapterHelloTriangleSub9  = "6.1.coordinate_systems"
+	ChapterHelloTriangleSub9  = "6.2.coordinate_systems_depth"
+	ChapterHelloTriangleSub9  = "6.3.coordinate_systems_multiple"
+	ChapterHelloTriangleSub9  = "6.4.coordinate_systems_exercise3"
+	ChapterHelloTriangleSub9  = ".1.camera_circle"
+	ChapterHelloTriangleSub9  = "7.2.camera_keyboard_dt"
+	ChapterHelloTriangleSub9  = "7.3.camera_mouse_zoom"
+	ChapterHelloTriangleSub9  = "7.4.camera_class"
+	ChapterHelloTriangleSub9  = "7.5.camera_exercise1"
+	ChapterHelloTriangleSub9  = "7.6.camera_exercise2"
+)
+
+const (
+	ChapterLighting1.colors/
+2.1.basic_lighting_diffuse/
+2.2.basic_lighting_specular/
+2.3.basic_lighting_exercise1/
+2.4.basic_lighting_exercise2/
+2.5.basic_lighting_exercise3/
+3.1.materials/
+3.2.materials_exercise1/
+4.1.lighting_maps_diffuse_map/
+4.2.lighting_maps_specular_map/
+4.3.lighting_maps_exercise2/
+4.4.lighting_maps_exercise4/
+5.1.light_casters_directional/
+5.2.light_casters_point/
+5.3.light_casters_spot/
+5.4.light_casters_spot_soft/
+6.multiple_lights/
+6.multiple_lights_exercise1/
+)
+
+const (
+1.model_loading/
+)
+const (
+1.1.depth_testing/
+1.2.depth_testing_view/
+10.1.instancing_quads/
+10.2.asteroids/
+10.3.asteroids_instanced/
+11.1.anti_aliasing_msaa/
+11.2.anti_aliasing_offscreen/
+2.stencil_testing/
+3.1.blending_discard/
+3.2.blending_sort/
+4.face_culling_exercise1/
+5.1.framebuffers/
+5.2.framebuffers_exercise1/
+6.1.cubemaps_skybox/
+6.2.cubemaps_environment_mapping/
+8.advanced_glsl_ubo/
+9.1.geometry_shader_houses/
+9.2.geometry_shader_exploding/
+9.3.geometry_shader_normals/
+
+)
+
+const (
+1.advanced_lighting/
+2.gamma_correction/
+3.1.1.shadow_mapping_depth/
+3.1.2.shadow_mapping_base/
+3.1.3.shadow_mapping/
+3.2.1.point_shadows/
+3.2.2.point_shadows_soft/
+3.3.csm/
+4.normal_mapping/
+5.1.parallax_mapping/
+5.2.steep_parallax_mapping/
+5.3.parallax_occlusion_mapping/
+6.hdr/
+7.bloom/
+8.1.deferred_shading/
+8.2.deferred_shading_volumes/
+9.ssao/
+
+)
+
+const (
+1.1.lighting/
+1.2.lighting_textured/
+2.1.1.ibl_irradiance_conversion/
+2.1.2.ibl_irradiance/
+2.2.1.ibl_specular/
+2.2.2.ibl_specular_textured/
+)
+
+const(
+1.debugging/
+3.2d_game/
+2.text_rendering/
+)
